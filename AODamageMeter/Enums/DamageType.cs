ad71824c5e57bab118c1df2d7f0dc64180975f23
David Galehouse <davghouse@gmail.com>
2017-04-23 14:32:52 -0400

    Supported missing damage types

--- AODamageMeter/Enums/DamageType.cs
+++ AODamageMeter/Enums/DamageType.cs
@@ -5,14 +5,27 @@ namespace AODamageMeter
 {
     public enum DamageType
     {
+        Unrecognized, // This shouldn't be used right now, but I want it here in case AO adds a new damage type.
+
         Chemical,
+        Cold,
         Energy,
+        Fire,
         Melee,
         Poison,
         Projectile,
         Radiation,
+        Unknown, // This is an actual damage type, unlike Unrecognized.
 
+        AimedShot,
+        Brawl,
+        Backstab,
         Burst,
+        Dimach,
+        FastAttack,
+        FlingShot,
+        FullAuto,
+        SneakAttack,
 
         Reflect,
         Shield,
@@ -21,17 +34,35 @@ namespace AODamageMeter
 
     public static class DamageTypeHelpers
     {
-        // For performance(?), use a dictionary rather than Enum.Parse's reflection.
         private static IReadOnlyDictionary<string, DamageType> _damageTypes = new Dictionary<string, DamageType>(StringComparer.OrdinalIgnoreCase)
         {
             ["Chemical"] = DamageType.Chemical,
+            ["Cold"] = DamageType.Cold,
             ["Energy"] = DamageType.Energy,
+            ["Fire"] = DamageType.Fire,
             ["Melee"] = DamageType.Melee,
             ["Poison"] = DamageType.Poison,
             ["Projectile"] = DamageType.Projectile,
             ["Radiation"] = DamageType.Radiation,
+            ["Unknown"] = DamageType.Unknown,
 
+            ["Aimed Shot"] = DamageType.AimedShot,
+            ["AimedShot"] = DamageType.AimedShot,
+            ["Brawl"] = DamageType.Brawl,
+            ["Brawling"] = DamageType.Brawl,
+            ["Backstab"] = DamageType.Backstab,
             ["Burst"] = DamageType.Burst,
+            ["Dimach"] = DamageType.Dimach,
+            ["Fast Attack"] = DamageType.FastAttack,
+            ["FastAttack"] = DamageType.FastAttack,
+            ["Fling Shot"] = DamageType.FlingShot,
+            ["FlingShot"] = DamageType.FlingShot,
+            ["Full Auto"] = DamageType.FullAuto,
+            ["FullAuto"] = DamageType.FullAuto,
+            ["Sneak Atck"] = DamageType.SneakAttack,
+            ["SneakAtck"] = DamageType.SneakAttack,
+            ["Sneak Attack"] = DamageType.SneakAttack,
+            ["SneakAttack"] = DamageType.SneakAttack,
 
             ["Reflect"] = DamageType.Reflect,
             ["Shield"] = DamageType.Shield,
@@ -39,6 +70,11 @@ namespace AODamageMeter
         };
 
         public static DamageType GetDamageType(string value)
-            => _damageTypes[value];
+        {
+            if (_damageTypes.TryGetValue(value, out DamageType damageType))
+                return damageType;
+
+            return DamageType.Unrecognized;
+        }
     }
 }
