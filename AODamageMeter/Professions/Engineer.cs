097914217202042223b7fafc7696b966b5086374
David Galehouse <davghouse@gmail.com>
2017-04-26 23:41:48 -0400

    Icon/color updates

--- AODamageMeter/Professions/Engineer.cs
+++ AODamageMeter/Professions/Engineer.cs
@@ -5,6 +5,6 @@ namespace AODamageMeter.Professions
     public class Engineer : Profession
     {
         public override string Name => "Engineer";
-        public override Color Color => Color.FromArgb(226, 84, 114);
+        public override Color Color => Color.FromArgb(133, 152, 170);
     }
 }
