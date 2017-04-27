bd1ff826bfa4d87ce4e0eb607ed65faf4bf6be5f
David Galehouse <davghouse@gmail.com>
2017-04-26 23:32:23 -0400

    Icon/color updates

--- AODamageMeter/Professions/Trader.cs
+++ AODamageMeter/Professions/Trader.cs
@@ -5,5 +5,6 @@ namespace AODamageMeter.Professions
     public class Trader : Profession
     {
         public override string Name => "Trader";
+        public override Color Color => Color.FromArgb(143, 72, 126);
     }
 }
