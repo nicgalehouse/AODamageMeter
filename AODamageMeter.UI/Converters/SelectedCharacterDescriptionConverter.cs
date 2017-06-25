e38d8a4ed9b94ed90b2266d6ebbfb14dc1cfd19c
David Galehouse <davghouse@gmail.com>
2017-06-24 23:08:25 -0400

    Show no character selected when only whitespace

---
+++ AODamageMeter.UI/Converters/SelectedCharacterDescriptionConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AODamageMeter.UI.Converters
+{
+    public class SelectedCharacterDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string characterName = (string)value;
+
+            return string.IsNullOrWhiteSpace(characterName) ? "No character selected" : $"{characterName} selected";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
